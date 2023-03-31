cb4b83ce36f1fac3bba6315252e9935045edbca4
tisthes <thaines@altny.net>
2023-03-31 00:47:35 -0400

    End of section 4

---
+++ API/DTOs/UserDto.cs
@@ -0,0 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace API.DTOs
+{
+  public class UserDto
+    {
+        public string UserName { get; set; }
+
+        public string Token { get; set; }
+    }
+}
