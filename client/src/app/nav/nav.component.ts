00b78eb4401749844b0d37da69e5433874691561
tisthes <thaines@altny.net>
2023-04-05 01:22:44 -0400

    End of Section 9

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { AccountService } from '../_services/account.service';
-import { Observable, of } from 'rxjs';
-import { User } from '../_models/user';
 import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 
@@ -14,7 +12,7 @@ export class NavComponent implements OnInit{
 
   model: any = {}
 
-  constructor(public accountService: AccountService, private router: Router, private toastr: ToastrService) {
+  constructor(public accountService: AccountService, private router: Router) {
     
   }
   ngOnInit(): void {
